b4b040a122bf5942a29e0ac26571b14b8af3f797
Eslashier <anfelov@gmail.com>
2021-10-02 09:28:26 -0500

    commit

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header> 
+        <a class="logo" href="Home.html">
+        <img src="../assets/1632184721717.jpg" alt="">
+        </a>
+
+        <nav>
+        <ul>
+            <li><a href="./Home.html">Inicio </a></li>
+            <li><a href="./Proyectos.html">Proyectos </a></li>
+            <li><a href="./About.html">Acerca de Nosotros </a></li>
+            <li><a href="./contact.html">Contáctenos </a></li>
+            <li><a href="./login.html">Iniciar sesión</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
